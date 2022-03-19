bca3aa37f084d6058477e46b86a3695552c1f3cf
purmirl <gmlrnjs1994@gmail.com>
2022-03-19 20:48:58 +0900

    20220319-01 : project start

---
+++ quiver.py
@@ -0,0 +1,20 @@
+"""
+Copyright 2022~ PeTrA. All rights reserved.
+Quiver Project : Simple Packet Capture Script with Python Scapy by PeTrA. 2022~
+Quiver 1.0
+Language : Python3.8.2 on pycharm IDE
+Library : Scapy2.4.3
+@quiver.py
+https://github.com/purmirl/PACKET-CAPTURE/quiver
+last update : 2022 MAR
+"""
+
+def packet_capture():
+    return
+
+def main():
+    packet_capture()
+    return
+
+if __name__ == "__main__":
+    main()
