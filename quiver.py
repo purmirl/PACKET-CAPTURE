9581582858b77ecd2882391489e8a4263f1324b2
purmirl <gmlrnjs1994@gmail.com>
2022-03-26 17:37:01 +0900

    20220326-01 : making packet capture

--- quiver.py
+++ quiver.py
@@ -1,5 +1,4 @@
 """
-Copyright 2022~ PeTrA. All rights reserved.
 Quiver Project : Simple Packet Capture Script with Python Scapy by PeTrA. 2022~
 Quiver 1.0
 Language : Python3.8.2 on pycharm IDE
@@ -8,12 +7,29 @@ Library : Scapy2.4.3
 https://github.com/purmirl/PACKET-CAPTURE/quiver
 last update : 2022 MAR
 """
+# from scapy.layers.inet import ICMP
+# from scapy.sendrecv import sniff
+from scapy.all import *
 
-def packet_capture():
+
+def packet_capture(_count, _prn, _filter):
+    sniff(prn = parsing_packet, filter = 'tcp')
+    # sniff(count = _count, prn = _prn, filter = _filter)
+    return
+
+def parsing_packet(_packet):
+    # packet = ICMP()
+    # packet.show()
+    _packet.show()
+    # a = _packet[Raw].load
+    # a.show()
+    print("break")
+    # print(result)
     return
 
 def main():
-    packet_capture()
+    packet_capture(1, parsing_packet, "UDP")
+    print("break")
     return
 
 if __name__ == "__main__":
